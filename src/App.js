5e4588df5353b26e0a50b96d754bc25c5e7e7244
stefan504 <63974903+stefan504@users.noreply.github.com>
2021-07-30 11:10:16 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [coins, setCoins] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(
+				'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=48&page=1&sparkline=false'
+			)
+			.then((res) => {
+				setCoins(res.data);
+				console.log(res.data);
+			});
+	}, []);
+
+	return (
+		<div className="App">
+			<Navbar />
+			<Dashboard coins={coins} />
+		</div>
+	);
 }
 
 export default App;
